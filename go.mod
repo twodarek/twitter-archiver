368e6fc41f4cc8b9fe587a3f9eb2140869cfbfcb
Thomas Wodarek <wodarekwebpage@gmail.com>
2022-11-15 00:16:16 -0500

    End of night checkpoint - add initial dep vendor and main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/twodarek/twitter-archiver
 
 go 1.19
+
+require (
+	github.com/dghubble/oauth1 v0.7.1
+	github.com/twodarek/go-twitter v0.0.0-20221115042815-bbc476854b8d
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dghubble/oauth1"
+	"github.com/twodarek/go-twitter/twitter"
+)
+
+func main() {
+	fmt.Printf("Starting download")
+
+	config := oauth1.NewConfig("consumerKey", "consumerSecret")
+	token := oauth1.NewToken("accessToken", "accessSecret")
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	// Twitter client
+	client := twitter.NewClient(httpClient)
+
+	truePtr := true
+	listParams := twitter.FavoriteListParams{
+		ScreenName:      "thomaswodarek",
+		Count:           200,
+		IncludeEntities: &truePtr,
+	}
+	favorites, res, err := client.Favorites.List(&listParams)
+	if err != nil || res.StatusCode > 299 {
+		fmt.Printf("Unable to get list of favorite tweets.  Error: %s", err)
+	}
+
+	usersToPullInfoFor := []twitter.User{}
+	for _, fav := range favorites {
+		usersToPullInfoFor = append(usersToPullInfoFor, *fav.User)
+	}
+}
